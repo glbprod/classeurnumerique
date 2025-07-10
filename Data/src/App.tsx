a6f2f4b06b55ae6cd6ca12db9861d5a877904dda
GLB <108488004+glbprod@users.noreply.github.com>
2025-07-10 21:00:06 +0200

    Create App.tsx

---
+++ Data/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarsBasisEmergencyData from './components/MarsBasisEmergencyData';
+
+function App() {
+  return (
+    <div className="App">
+      <MarsBasisEmergencyData />
+    </div>
+  );
+}
+
+export default App;
